baad6a1e079ce0140b196adfa3edb6f914d617e9
Shannon Makenna <shannonkariuki@gmail.com>
2019-04-08 12:30:46 +0200

    changed start script to node server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 
 const db = knex({
 	client: 'pg',
-  connection: {
+    connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl:true,
+      ssl:true,
 	}
 });
 
