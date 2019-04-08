3ad55b40d74ba926218e02dedd29866a6f8964c5
Shannon Makenna <shannonkariuki@gmail.com>
2019-04-08 21:29:26 +0200

    put actual URL as connection string

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
     connection: {
-    connectionString : process.env.DATABASE_URL,
+    connectionString : postgres://feztpmdruenffa:3e00d3dabe733fa25a24ecdfeaff293a9745c7159e4c3af7167ce47d7b48bd4f@ec2-54-225-129-101.compute-1.amazonaws.com:5432/d70dnfm9qqlluo
       ssl:true,
 	}
 });
@@ -31,7 +31,7 @@ app.get('/profile/:id', (req, res) =>{profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 app.listen(process.env.PORT || 3001, () => {
-	console.log(`app is running on port ${process.env.PORT}`);
+	console.log( process.env.DATABASE_URL);
 });
 
 
